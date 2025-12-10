6839e0002156da57878fad63b89b28e087e21dbd
filip <filip@baving@users.noreply.github.com>
2025-12-10 11:51:58 +0100

    initial node.js and express setup

---
+++ server.js
@@ -0,0 +1,12 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const PORT = 3000; 
+
+// Tell Express to serve client files from the 'public' directory
+app.use(express.static('public')); 
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
